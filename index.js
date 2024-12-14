d85a4addc37be414291ea147e5e95d70c4cc4b51
Terry Leggett II <terryleggettjr@gmail.com>
2024-12-14 17:33:53 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
